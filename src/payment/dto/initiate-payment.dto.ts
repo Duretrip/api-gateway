b026a3d2e6879fd1db35ecada6d2539dfe375f5a
Dare Samuel <dawakesam@gmail.com>
2023-11-22 14:59:12 +0100

    Payment added to API Gateway

---
+++ src/payment/dto/initiate-payment.dto.ts
@@ -0,0 +1,58 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsObject, IsString } from 'class-validator';
+
+export class InititatePaymentDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'tx_li_$#%#ng***' })
+  tx_ref: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({ example: 1000 })
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'NG' })
+  currency: string;
+
+  @IsObject()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: {
+      entity_id: 1,
+      entity_type: 'hotel',
+      user_id: 1,
+    },
+  })
+  meta: {
+    entity_id: number;
+    entity_type: string;
+    user_id: number;
+  };
+
+  @IsObject()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: {
+      email: 'customer@example.com',
+      name: 'customer name',
+    },
+  })
+  customer: {
+    email: string;
+    name: string;
+  };
+
+  @IsObject()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: {
+      title: 'Hotel Payment',
+    },
+  })
+  customizations: {
+    title: string;
+  };
+}
